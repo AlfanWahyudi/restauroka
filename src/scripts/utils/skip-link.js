ab81eafadbf54110d6baa1614f2d7ca6f5c05986
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2022-05-17 14:49:12 +0700

    Remove unproduction file for github pages

--- src/scripts/utils/skip-link.js
+++
@@ -1,8 +0,0 @@
-const skipLink = (skipLinkElem, destination) => {
-  skipLinkElem.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.querySelector(destination).focus();
-  });
-};
-
-export default skipLink;
